ea891adc42492acaa98e980ccb61aa69662aa909
octaviav <octavia7104@gmail.com>
2023-10-05 10:31:04 -0700

    c print function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - this is a prinf function
+ * main - this is a printf function
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf ("with proper grammer, but the outcome is a piece of art,\n");
 	return(0);
 }
