121fb626f76ea2e4434a6e6c6745e4b751d8a023
octaviav <octavia7104@gmail.com>
2023-10-10 12:20:26 -0700

    alphabet x10

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alphabet
+ * Return: void here
+ */
+
+void print_alphabe(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		{
+		_putchar(c);
+		}
+		_putchar('\n');
+}
