013337294ae1b907ed171983fc0d6dcf11fda355
octaviav <octavia7104@gmail.com>
2023-10-23 11:48:30 -0700

    single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - program that prints the single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+
+		putchar('\n');
+
+	return (0);
+}
