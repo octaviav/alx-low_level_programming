34b8de073530e2bef5cfe32eb62d324d9bc9efb9
octaviav <octavia7104@gmail.com>
2023-10-24 06:17:36 -0700

    Hexidecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+ * main - main function
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	int i;
+
+	c = 'a';
+	i = 0;
+	while
+		(i < 10){
+			putchar(i + '0');
+			(i++);
+		}
+	while 
+		(c <= 'f') {
+			putchar(c);
+			(c++);
+		}
+	putchar('\n');
+	return (0);
+}
