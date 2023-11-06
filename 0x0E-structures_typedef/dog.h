4f7577f9a4e7fd699b88bb55eed5f7ea86f0cbbb
octaviav <octavia7104@gmail.com>
2023-11-06 11:19:07 -0800

    new type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,8 +6,6 @@
  * @name - dog name
  * @age - dog age
  * @owner - dog owner
- *
- *dog_t - typedef for struct dog
  */
 	struct dog
 {
@@ -15,6 +13,9 @@
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - typedef for struct dog
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
