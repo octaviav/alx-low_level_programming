238834ef3ed5a3e0db5c6b1aeb4a1bcbe8c53dfe
octaviav <octavia7104@gmail.com>
2023-11-21 09:00:35 -0800

    sum of data of listint_t

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * get_nodeint_at_index - returns the node at a certain index in a linked list
+ * @head: first node in linked list
+ * @index: index of the node to return
+ *
+ * Return: pointer to the node or NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *temp = head;
+
+	while (temp && i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	return (temp ? temp : NULL);
+}
