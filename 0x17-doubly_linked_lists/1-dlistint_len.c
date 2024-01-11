ddb87bcd799b1849b87f8736aaf76ea0347d9eb9
octaviav <octavia7104@gmail.com>
2024-01-11 08:57:51 -0800

    number of elements in a linked dlisint_t

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns number of elements in dlistint
- * @head: head of the list
+ * dlistint_len - calculates the length of doubly linked lists
+ * @h: head of the list
  * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
